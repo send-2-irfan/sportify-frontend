33c4c64a4e6e9716cb0e33379351c69c3dc4c15d
send-2-irfan <irfanullah.bses19@iba-suk.edu.pk>
2023-02-13 12:58:32 +0500

    Event module modified

--- src/layouts/manageEvents/Events.js
+++ src/layouts/manageEvents/Events.js
@@ -17,6 +17,9 @@ import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import {openNotificationWithIcon} from "../../components/global/notification";
+import ArgonButton from "../../components/ArgonButton";
+import PlaceholderCard from "../../examples/Cards/PlaceholderCard";
+import {useNavigate} from "react-router-dom";
 
 const ITEM_HEIGHT = 48;
 const ITEM_PADDING_TOP = 8;
@@ -49,11 +52,20 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 export default function Events() {
+    const [open, setOpen] = useState(false);
     const {sports, coordinators} = useContext(ApplicationContext)
     const [coordinatorName, setCoordinatorName] = React.useState('');
     const {allEvents, setAllEvents} = useContext(ApplicationContext)
 
     const [sportNames, setSportNames] = React.useState([]);
+
+    const handleOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
 
     const handleChangeSportNames = (event) => {
         const {target: {value},} = event;
@@ -101,6 +113,7 @@ export default function Events() {
         JSON.parse(localStorage.getItem("events")) && setAllEvents(JSON.parse(localStorage.getItem("events")))
     }, [])
 
+    const navigate = useNavigate();
 
     return (
         <>
@@ -137,80 +150,109 @@ export default function Events() {
                                             />
                                             <div className={classes.cardActions}>
                                                 <button onClick={showModal} className="btn btn-outline-primary btn-sm">
-                                                    Modify Event
+                                                    Manage Event
                                                 </button>
                                             </div>
                                             {/* dropdown menu ends here*/}
 
 
-                                            <Modal width={'40%'} title="Modify Event" open={isModalOpen}
-                                                   onOk={(events) => handleOk(events)}
+                                            <Modal title="Manage Sports and Coordinators" open={isModalOpen}
+                                                   // onOk={}
                                                    style={{
                                                        marginTop: '12%',
                                                    }}
                                                    onCancel={handleCancel}>
 
                                                 <div className="row mb-4">
-                                                    <div className="col-sm-12 col-lg-6 col-xl-6 col-md-12">
-                                                        <label htmlFor="coodinator select">Select Sports for
-                                                            Event</label>
-                                                        <div>
-                                                            <FormControl sx={{m: 1, minWidth: 200}} size="small">
-                                                                <InputLabel
-                                                                    id="demo-select-large">Coordinator</InputLabel>
-                                                                <Select
-                                                                    labelId="demo-select-large"
-                                                                    id="demo-select-large"
-                                                                    value={coordinatorName}
-                                                                    label="Age"
-                                                                    onChange={handleChangeCoordinatorName}
-                                                                >
-                                                                    {
-                                                                        coordinators.map(coordinator => {
-                                                                            return <MenuItem key={coordinator.fullName}
-                                                                                             value={coordinator.fullName}>
-                                                                                <em>{coordinator.fullName}</em>
-                                                                            </MenuItem>
-                                                                        })
-                                                                    }
-                                                                </Select>
-                                                            </FormControl>
+
+                                                        {/*<label htmlFor="Manage Sports">Select Sports for*/}
+                                                        {/*    Event</label>*/}
+                                                        <div className={"col-sm-12 col-lg-6 col-xl-6 col-md-12"}>
+                                                            <Grid onClick={()=>navigate('/manage-sports')} style={{width:'200px'}}>
+                                                                <PlaceholderCard title={{variant: "h5", text: "Add Sports"}} outlined/>
+                                                            </Grid>
                                                         </div>
-                                                    </div>
-                                                    <div className="col-sm-12 col-lg-6 col-xl-6 col-md-12">
-                                                        <label htmlFor="coodinator select">Select Coordinators for
-                                                            Event</label>
-                                                        <div>
-                                                            <FormControl sx={{m: 1, width: 300}}>
-                                                                <InputLabel
-                                                                    id="demo-multiple-name-label">Sports</InputLabel>
-                                                                <Select
-                                                                    labelId="demo-multiple-name-label"
-                                                                    id="demo-multiple-name"
-                                                                    multiple
-                                                                    value={sportNames}
-                                                                    onChange={handleChangeSportNames}
-                                                                    input={<OutlinedInput label="Name"/>}
-                                                                    MenuProps={MenuProps}
-                                                                >
-                                                                    {
-                                                                        sports.map(sport => {
-                                                                            return <MenuItem key={sport.sportName}
-                                                                                             value={sport.sportName}>
-                                                                                <em>{sport.sportName}</em>
-                                                                            </MenuItem>
-                                                                        })
-                                                                    }
-                                                                </Select>
-                                                            </FormControl>
+                                                        <div className={"col-sm-12 col-lg-6 col-xl-6 col-md-12"}>
+                                                            <Grid onClick={()=>navigate('/manage-coordinator')} style={{width:'200px'}}>
+                                                                <PlaceholderCard title={{variant: "h5", text: "Add Coordinator"}} outlined/>
+                                                            </Grid>
                                                         </div>
 
                                                     </div>
-                                                    <div className="col-sm-12 col-lg-6 col-xl-6 col-md-12">
-                                                        {/*   drop down 2*/}
-                                                    </div>
-                                                </div>
                                             </Modal>
+
+
+
+
+
+                                            {/*<Modal width={'40%'} title="Modify Event" open={isModalOpen}*/}
+                                            {/*       onOk={(events) => handleOk(events)}*/}
+                                            {/*       style={{*/}
+                                            {/*           marginTop: '12%',*/}
+                                            {/*       }}*/}
+                                            {/*       onCancel={handleCancel}>*/}
+
+                                            {/*    <div className="row mb-4">*/}
+                                            {/*        <div className="col-sm-12 col-lg-6 col-xl-6 col-md-12">*/}
+                                            {/*            <label htmlFor="coodinator select">Select Sports for*/}
+                                            {/*                Event</label>*/}
+                                            {/*            <div>*/}
+                                            {/*                <FormControl sx={{m: 1, minWidth: 200}} size="small">*/}
+                                            {/*                    <InputLabel*/}
+                                            {/*                        id="demo-select-large">Coordinator</InputLabel>*/}
+                                            {/*                    <Select*/}
+                                            {/*                        labelId="demo-select-large"*/}
+                                            {/*                        id="demo-select-large"*/}
+                                            {/*                        value={coordinatorName}*/}
+                                            {/*                        label="Age"*/}
+                                            {/*                        onChange={handleChangeCoordinatorName}*/}
+                                            {/*                    >*/}
+                                            {/*                        {*/}
+                                            {/*                            coordinators.map(coordinator => {*/}
+                                            {/*                                return <MenuItem key={coordinator.fullName}*/}
+                                            {/*                                                 value={coordinator.fullName}>*/}
+                                            {/*                                    <em>{coordinator.fullName}</em>*/}
+                                            {/*                                </MenuItem>*/}
+                                            {/*                            })*/}
+                                            {/*                        }*/}
+                                            {/*                    </Select>*/}
+                                            {/*                </FormControl>*/}
+                                            {/*            </div>*/}
+                                            {/*        </div>*/}
+                                            {/*        <div className="col-sm-12 col-lg-6 col-xl-6 col-md-12">*/}
+                                            {/*            <label htmlFor="coodinator select">Select Coordinators for*/}
+                                            {/*                Event</label>*/}
+                                            {/*            <div>*/}
+                                            {/*                <FormControl sx={{m: 1, width: 300}}>*/}
+                                            {/*                    <InputLabel*/}
+                                            {/*                        id="demo-multiple-name-label">Sports</InputLabel>*/}
+                                            {/*                    <Select*/}
+                                            {/*                        labelId="demo-multiple-name-label"*/}
+                                            {/*                        id="demo-multiple-name"*/}
+                                            {/*                        multiple*/}
+                                            {/*                        value={sportNames}*/}
+                                            {/*                        onChange={handleChangeSportNames}*/}
+                                            {/*                        input={<OutlinedInput label="Name"/>}*/}
+                                            {/*                        MenuProps={MenuProps}*/}
+                                            {/*                    >*/}
+                                            {/*                        {*/}
+                                            {/*                            sports.map(sport => {*/}
+                                            {/*                                return <MenuItem key={sport.sportName}*/}
+                                            {/*                                                 value={sport.sportName}>*/}
+                                            {/*                                    <em>{sport.sportName}</em>*/}
+                                            {/*                                </MenuItem>*/}
+                                            {/*                            })*/}
+                                            {/*                        }*/}
+                                            {/*                    </Select>*/}
+                                            {/*                </FormControl>*/}
+                                            {/*            </div>*/}
+
+                                            {/*        </div>*/}
+                                            {/*        <div className="col-sm-12 col-lg-6 col-xl-6 col-md-12">*/}
+                                            {/*            /!*   drop down 2*!/*/}
+                                            {/*        </div>*/}
+                                            {/*    </div>*/}
+                                            {/*</Modal>*/}
                                         </Grid>
                                     })
                                 }
